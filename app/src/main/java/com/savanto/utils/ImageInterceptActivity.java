14f74eb7966e257cf77fda7da2154668e3a7fdd2
savanto <savanto@users.noreply.github.com>
2016-07-08 11:47:56 -0700

    Add view image Intent interception

---
+++ app/src/main/java/com/savanto/utils/ImageInterceptActivity.java
@@ -0,0 +1,13 @@
+package com.savanto.utils;
+
+import android.app.Activity;
+
+/**
+ * Intercept {@code android.intent.action.VIEW} Intents from Gmail to workaround bug:
+ * if there's no Activity that can respond to view image Intents, Gmail cannot show it, but if an
+ * Activity can respond, Gmail can actually show the image no problem.
+ *
+ * This can be expanded later to handle viewing images by other means.
+ */
+public class ImageInterceptActivity extends Activity {
+}
